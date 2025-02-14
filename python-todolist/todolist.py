ec9d6efc50626a2aca4c2f991a6f794575b17176
r3dkoi <188728180+r3dkoi@users.noreply.github.com>
2025-02-14 08:22:47 +0800

    1. began on adding tasks 2. starting proper pseudocode

---
+++ python-todolist/todolist.py
@@ -0,0 +1,27 @@
+def main():
+    """Holds all our tasks
+    A while True loop used to keep program running until
+    user decides to exit.
+
+    User is prompted to input their choice
+    """
+    tasks = []
+    while True:
+        print("\n==== To-Do List ====")
+        print("1. Add tasks")
+        print("2. Show tasks")
+        print("3. Mark tasks as done")
+        print("4. Delete tasks")
+        print("5. Exit")
+
+        choice = input("Enter your choice: ")
+
+        #Adding tasks section
+        if choice == '1':
+            print()
+            n_tasks = int(input("How many tasks do you want to add: "))
+
+            for i in range(n_tasks):
+                task = input("Enter the task: ")
+                tasks.append({"task": task, "done": False})
+
