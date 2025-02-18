31d88435352d7824190c7d798ab8369f5882d6cb
r3dkoi <188728180+r3dkoi@users.noreply.github.com>
2025-02-18 14:30:14 +0800

    renamed because hyphens not accepted

---
+++ python-todolist-2/functions_2.py
@@ -0,0 +1,19 @@
+"""CORE Functions file for the to-do list
+"""
+
+def adding_tasks(tasks): #Will be altering the tasks variable
+    print("/n ==== ADDING TASKS ====")
+    while True:
+        try:
+            n_tasks = int(input("How many tasks would you like to add: "))
+            break #Ends while loop and disregards except block
+        except ValueError:
+            print("Please enter a valid number.")
+    for i in range(n_tasks): #Creates task list that ends on amount user had inputted
+        task = print("Please enter task description").lower
+        tasks.task({
+            "Task:", task,
+            "done:", False
+        }) #Status is set to false initially.
+
+        
