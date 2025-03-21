22080ebe65c060b02a2802b5df88e6620a292461
r3dkoi <188728180+r3dkoi@users.noreply.github.com>
2025-03-21 21:32:11 +0800

    added ttk widgets

--- tkinter_tinkering/displaying_window.py
+++ tkinter_tinkering/displaying_window.py
@@ -12,8 +12,17 @@ root.title('Tkinter Window Demo')
  #Changes window size and the position to 50 px from top and left of screen
 root.geometry('600x400+50+50')
 
+#Ttk label testing
+ttk.Button(root, text='Button').pack()
+ttk.Checkbutton(root, text='Checkbutton').pack()
+ttk.Entry(root, text='Entry').pack()
+ttk.Menubutton(root, text='Menubutton').pack()
+ttk.Radiobutton(root, text="Radiobutton").pack()
+ttk.Spinbox(root, text="Spinbox").pack()
+ttk.Combobox(root, text="Combobox").pack()
 
-#Places a label on the root window
+
+#Places a classic label on the root window
 message = tk.Label(root, text="Window Display Testing :)")
 message.pack() 
 
