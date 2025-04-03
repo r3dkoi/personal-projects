cb8e183e0041dca9c3c5217c96d6b3c9ec53d590
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-03 16:41:31 +0800

    fix: Update foreign key comment and set ProductID as primary key in Instrument Inventory table

--- Databases/cssd_inventory/product_instruments.py
+++ Databases/cssd_inventory/product_instruments.py
@@ -11,21 +11,19 @@ cursor = conn.cursor()
 
 # Creating Product_Inventory Table 
 
-#Don't forget to add the foreign keys to the table once the Product and Location tables are created
+#Don't forget to add the foreign keys to the inventory table once the Product and Location tables are created
 # FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
 # FOREIGN KEY (LocationID) REFERENCES Locations(LocationID)
 # FOREIGN KEY (SupplierID) REFERENCES Supplers(SupplierID)    
 cursor.execute("""
              CREATE TABLE `Instrument Inventory` (
-                InventoryID int AUTO_INCREMENT PRIMARY KEY,
-                ProductID int,
+                ProductID int AUTO_INCREMENT PRIMARY KEY,
                 Name_and_Description varchar(255),
                 Category varchar(255),
                 Cost int,
                 SupplierID int,
                 SKU int,
-                Sterilisation_Method varchar(255),
-                FOREIGN KEY (ProductID) REFERENCES inventory(InventoryID)
+                Sterilisation_Method varchar(255)
              );
              """)
 conn.commit()
