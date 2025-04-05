2d36232340f635a0272ef5d2bd9835f8de75fecf
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-05 19:46:55 +0800

    refactor: Change LocationID column to StorageLocation in inventory table for improved clarity

--- Databases/cssd_inventory/inventory.py
+++ Databases/cssd_inventory/inventory.py
@@ -17,10 +17,18 @@ cursor = conn.cursor()
 # FOREIGN KEY (LocationID) REFERENCES Locations(LocationID)    
 cursor.execute("""
                ALTER TABLE `inventory` 
-               DROP COLUMN `ExpiryDate`
+               ALGORITHM=INPLACE,
+               CHANGE `LocationID` `StorageLocation` VARCHAR(255);
                """)
 conn.commit()
 conn.close()  # closes my  connection when done
 
+""" 
+VALUES (482038, 70, 9295860c, 150, 2025-03-06)
+VALUES (485285, 5,  2ec486fc, 10, 2025-03-21)
+               VALUES (485285, 100, 632ab1dc, 200, 2025-03-21)
+               VALUES (307331, 15, 2ec486fc, 25, 2025-02-19)
+               VALUES (307331, 0,  9295860c, 8, 2025-02-19)
+               VALUES (236314, 44, 632ab1dc, 120, 2025-03-16)"""
 
 
