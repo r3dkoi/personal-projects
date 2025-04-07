786fd70fe57062a992665b5eedf716b060b0ce27
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-07 12:41:19 +0800

    refactor: Update contact numbers for existing suppliers to ensure accurate supplier information

--- Databases/cssd_inventory/suppliers.py
+++ Databases/cssd_inventory/suppliers.py
@@ -15,13 +15,19 @@ cursor = conn.cursor()
 # FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
 # FOREIGN KEY (LocationID) REFERENCES Locations(LocationID)
 # FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID) in instruments inventory table
-cursor.execute("""
-               INSERT INTO `suppliers` (`SupplierID`, `Supplier_Name`, `Contact_Number`, `Contact_Email`, `Address`, `Opening Hours`)
-               VALUES
-                  (5711, "Life Healthcare Pty Ltd", 1800-060-168, "orders@lifehealthcare.com.au", "Level 2/327 Cambridge St, Wembley WA 6014", "Monday - Friday from 8:30 AM – 5:30 PM"),
-                  (4775, "Medtronic Australasia Pty Ltd", 08-6389-5600, "australia.diabetes@medtronic.com", "Medtronic Australasia Pty Ltd, Lvl 3/1060 Hay St, West Perth WA 6005", "Monday - Friday from 8:30 AM – 5:30 PM"),
-                  (1761, "Abacus dx Pty Ltd", 1800-222-287, "orders@abacusdx.com", "34 Corporate Drive, Cannon Hill, QLD, 4170", "Monday - Friday from 8:30 AM - 5:00pm");
-             """)
+
+#Updating Phone Numbers
+updates = [
+    ('1800-060-168', 5711),
+    ('08-6389-5600', 4775),
+    ('1800-222-287', 1761)
+]
+for phone_number, supplier_id in updates:
+    cursor.execute("""
+               UPDATE `suppliers`
+               SET `Contact_Number` = %s
+               WHERE `SupplierID` = %s;
+               """, (phone_number, supplier_id))
 conn.commit()
 conn.close()  # closes my  connection when done
 
