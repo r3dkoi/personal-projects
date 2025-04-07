768806f7ad992bc443b4c8a5fcb0fc2fb5f1b686
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-07 12:46:33 +0800

    refactor: Update SupplierID for multiple products in instrument inventory to ensure accurate supplier association

--- Databases/cssd_inventory/product_instruments.py
+++ Databases/cssd_inventory/product_instruments.py
@@ -15,14 +15,21 @@ cursor = conn.cursor()
 # FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
 # FOREIGN KEY (LocationID) REFERENCES Locations(LocationID)
 # FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID) in instruments inventory table
-cursor.execute("""
-             INSERT INTO `Instrument Inventory` (`ProductID`, `Name`, `Intended Purpose`, `Category`, `Cost`, `SKU`, `Sterilisation_Method`, `Class`, `ShelfLife`)
-               VALUES 
-                    (482038, "Gillies Dissecting Forcep", "Grasping delicate tissues with its narrow jaws providing comfortable access to deep tissues", "RMD", 34.10, 465469, "Steam", "I", "Used daily"),
-                    (485285, "Disposable Gillies Dissecting Forceps", "Grasping delicate tissues with its narrow jaws providing comfortable access to deep tissues", "Single Use", 6.00, 883967, "N/A", "I", "N/A"),
-                    (307331, "Pacing Leads", "Specialised conductors used in cardiac rhythm management devices to deliver electrical impulses from a pacemaker to the heart", "RMD", 597.93, 900654, "Hydrogen Peroxide", "III", "4 Years"),
-                    (236314, "IVD Packs", "Combinations of products packaged together for specific diagnostic purposes", "IVD", 300.00, 945732, "None", "2", "4-6 Months");            
-             """)
+
+#Updating Supplier ID in product_instruments table
+updates = [
+    (1761, 236314),
+    (4775, 307331),
+    (5711, 482038),
+    (5711, 485285)
+]
+
+for supplier_id, product_id in updates:
+    cursor.execute("""
+               UPDATE `instrument inventory`
+               SET `SupplierID` = %s
+               WHERE `ProductID` = %s;
+               """, (supplier_id, product_id))
 conn.commit()
 conn.close()  # closes my  connection when done
 
