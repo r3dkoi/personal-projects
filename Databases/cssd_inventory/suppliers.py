46465e6873affa18b78a7dbf50e4c61c9a62a924
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-07 12:37:12 +0800

    refactor: Insert multiple new suppliers into suppliers table to enhance supplier management

--- Databases/cssd_inventory/suppliers.py
+++ Databases/cssd_inventory/suppliers.py
@@ -16,14 +16,11 @@ cursor = conn.cursor()
 # FOREIGN KEY (LocationID) REFERENCES Locations(LocationID)
 # FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID) in instruments inventory table
 cursor.execute("""
-             CREATE TABLE `Suppliers` (
-                SupplierID int AUTO_INCREMENT PRIMARY KEY,
-               Supplier_Name varchar(255),
-               Contact_Person varchar(255),
-               Contact_Number int,
-               Contact_Email varchar(255),
-               Address varchar(255)
-             );
+               INSERT INTO `suppliers` (`SupplierID`, `Supplier_Name`, `Contact_Number`, `Contact_Email`, `Address`, `Opening Hours`)
+               VALUES
+                  (5711, "Life Healthcare Pty Ltd", 1800-060-168, "orders@lifehealthcare.com.au", "Level 2/327 Cambridge St, Wembley WA 6014", "Monday - Friday from 8:30 AM – 5:30 PM"),
+                  (4775, "Medtronic Australasia Pty Ltd", 08-6389-5600, "australia.diabetes@medtronic.com", "Medtronic Australasia Pty Ltd, Lvl 3/1060 Hay St, West Perth WA 6005", "Monday - Friday from 8:30 AM – 5:30 PM"),
+                  (1761, "Abacus dx Pty Ltd", 1800-222-287, "orders@abacusdx.com", "34 Corporate Drive, Cannon Hill, QLD, 4170", "Monday - Friday from 8:30 AM - 5:00pm");
              """)
 conn.commit()
 conn.close()  # closes my  connection when done
