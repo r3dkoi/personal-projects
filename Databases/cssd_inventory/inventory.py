27799c297220f480a5bb0481fd03a75ac97de109
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-07 11:38:44 +0800

    refactor: Update LocationID for multiple inventory items to ensure accurate location association

--- Databases/cssd_inventory/inventory.py
+++ Databases/cssd_inventory/inventory.py
@@ -12,25 +12,24 @@ cursor = conn.cursor()
 
 #Don't forget to add the foreign keys to the table once the Product and Location tables are created
 # FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
-# FOREIGN KEY (LocationID) REFERENCES Locations(LocationID)    
+# FOREIGN KEY (LocationID) REFERENCES Locations(LocationID) 
 
-# Update ProductID for different inventory items
+#Update LocationID for different inventory items
 updates = [
-    (485285, 3),
-    (485285, 4),
-    (307331, 5),
-    (307331, 6),
-    (236314, 7)
+    (2459273000, 2),
+    (1663742000, 3),
+    (784631548, 4),
+    (1663742000, 5),
+    (2459273000, 6),
+    (784631548, 7)
 ]
-
-for product_id, inventory_id in updates:
+for location_id, inventory_id in updates:
     cursor.execute("""
-        UPDATE `inventory`
-        SET `ProductID` = %s
-        WHERE `InventoryID` = %s
-    """, (product_id, inventory_id))
-    conn.commit()
-
-conn.close()  # closes my connection when done
+               UPDATE `inventory`
+               SET `LocationID` = %s
+               WHERE `InventoryID` = %s;
+               """, (location_id, inventory_id))
+conn.commit()
+conn.close()  # closes my  connection when done
 
 
