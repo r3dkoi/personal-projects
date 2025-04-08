d4ae40440a01d53d300da19526ff28306ce2f7d6
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-08 10:19:25 +0800

    refactor: Add foreign key constraint to RoleID in users table for improved data integrity

--- Databases/cssd_inventory/user.py
+++ Databases/cssd_inventory/user.py
@@ -11,16 +11,10 @@ conn = mysql.connector.connect(
 #Don't forget to add foreign key to RoleID to reference RoleID in roles table
 
 cursor = conn.cursor() 
-cursor.execute("""CREATE TABLE `users` (
-               `UserID` INT PRIMARY KEY AUTO_INCREMENT,
-               `EmployeeID` INT NOT NULL,
-               `RoleID` INT NOT NULL, 
-               `Password` TEXT NOT NULL,
-               `FullName` VARCHAR(50) NOT NULL,
-               `Department` VARCHAR(50) NOT NULL,
-               `LastLogin` DATETIME NOT NULL,
-               `AccountStatus` TEXT NOT NULL
-               )""")
+cursor.execute("""
+               ALTER TABLE `users`
+               ADD FOREIGN KEY (`RoleID`) REFERENCES `roles`(`RoleID`)
+               """)
 conn.commit()
 conn.close()  # closes my  connection when done
 
