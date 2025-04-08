91873a392a3a1449a4d9e919667de748a532bddb
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-08 09:59:02 +0800

    refactor: Insert initial role entries into roles table for user role management

--- Databases/cssd_inventory/roles.py
+++ Databases/cssd_inventory/roles.py
@@ -11,12 +11,15 @@ conn = mysql.connector.connect(
 
 
 cursor = conn.cursor() 
-cursor.execute("""CREATE TABLE `roles`(
-               `RoleID` INT(11) PRIMARY KEY AUTO_INCREMENT,
-               `RoleName` VARCHAR(50) NOT NULL,
-               `Description` TEXT NOT NULL,
-               `ActiveStatus` BOOLEAN NOT NULL DEFAULT 1
-               )""")
+cursor.execute("""
+               INSERT INTO `roles`(`RoleName`, `Description`, `ActiveStatus`)
+               VALUES
+               ("Sterile Services Manager", "Edit instrument records. Has access to staff records, salary, hospital departments, vendors, sterilisers, washers, preparation areas, theatres", 1),
+               ("Sterile Services Team Leader", "View and edit records Does not have access to staff records, other departments, theatre records. Can issue repairs and restocking inventory", 1),
+               ("Sterile Services Employee", "Print barcodes and scan items,  Process and pass/fail loads, does not have access to staff records and inventory records", 1), 
+               ("Inventory Administrator", "Access the inventory table records. Able to order and restock inventory.", 1),
+               ("Guest", "None", 0)
+               """)
 conn.commit()
 conn.close()  # closes my  connection when done
 
