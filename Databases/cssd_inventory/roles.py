5778720c43bbb035d92808f36b504de7dd0e2054
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-10 12:31:14 +0800

    refactor: Update roles.py to grant privileges for 'Sterile Services Manager' role in the roles table

--- Databases/cssd_inventory/roles.py
+++ Databases/cssd_inventory/roles.py
@@ -8,17 +8,12 @@ conn = mysql.connector.connect(
     database='cssd_inventory'  # must match the name you created in phpMyAdmin 
 ) 
 #Table for user roles
+#Granting privileges to the roles
 
 
 cursor = conn.cursor() 
 cursor.execute("""
-               INSERT INTO `roles`(`RoleName`, `Description`, `ActiveStatus`)
-               VALUES
-               ("Sterile Services Manager", "Edit instrument records. Has access to staff records, salary, hospital departments, vendors, sterilisers, washers, preparation areas, theatres", 1),
-               ("Sterile Services Team Leader", "View and edit records Does not have access to staff records, other departments, theatre records. Can issue repairs and restocking inventory", 1),
-               ("Sterile Services Employee", "Print barcodes and scan items,  Process and pass/fail loads, does not have access to staff records and inventory records", 1), 
-               ("Inventory Administrator", "Access the inventory table records. Able to order and restock inventory.", 1),
-               ("Guest", "None", 0)
+               GRANT SELECT, INSERT, UPDATE, DELETE;
                """)
 conn.commit()
 conn.close()  # closes my  connection when done
