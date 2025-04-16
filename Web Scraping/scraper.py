13840f6602cb57bd563db0f89a7deb567206fb21
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-16 19:10:52 +0800

    refactor: Simplify lesson plan extraction logic in scraper.py by consolidating code and removing unnecessary comments

--- Web Scraping/scraper.py
+++ Web Scraping/scraper.py
@@ -13,29 +13,39 @@ def get_lesson_plans(lesson_url: str) -> dict:
     #Create an empty lesson plan details dictionary
     lesson_plans = {}
 
+    page = requests.get(lesson_url, headers=headers)
+    soup = BeautifulSoup(page.content, features='lxml')
+
+    try:
+        #Extracting title of lesson plan
+        lesson_title = soup.find('div', class="small-12 medium-9 columns")
+        if lesson_title:
+            title_element = lesson_title.find('h2')
+            if title_element:
+                title = title_element.text.strip()
+
+        #Extracting Subjects of lesson plan
+        subjects = soup.find('div', class="small-12 columns")
+        if subjects:
+            subject_element = subjects.find('p')
+            if subject_element:
+                subject = subject_element.text.strip()
+
+        #Adding data to the lesson plan dictionary
+        lesson_plans['title'] = title
+        lesson_plans['subject'] = subject
+
+        #Return the lesson plan dictionary
+        return lesson_plans
+    except Exception as e:
+        print("Error: Unable to retrieve lesson plan details.")
+        print(f'Failed with exception: {e}')
+    
+#Prompting user
+product_url = input("Enter the URL of the lesson plan: ")
+lesson_plans = get_lesson_plans(product_url)
+
+print(lesson_plans)
+
     
 
-#Get lesson plan page content and create a soup
-page = requests.get(lesson_url, headers=headers)
-soup = BeautifulSoup(page.content, features='lxml')
-
-#Extracting the data from page section
-"""This is where I have to view the HTML code of the webpage.
-I can use the inspect tool in the browser to view the HTML code.
-Alternatively I can choose Inspect to access the Developer tools and make sure I'm in the Elements tab to see the HTML structure."""
-
-#Extracting title of lesson plan
-lesson_title = soup.find('div', class="small-12 medium-9 columns")
-if lesson_title:
-    title_element = title_div.find('h2')
-    if title_element:
-        title = title_element.text.strip()
-        lesson_plans['title'] = title
-
-#Extracting Subjects of lesson plan
-subjects = soup.find('div', class="small-12 columns")
-if subjects:
-    subject_element = subjects.find('p')
-    if subject_element:
-        subject = subject_element.text.strip()
-        lesson_plans['subject'] = subject
