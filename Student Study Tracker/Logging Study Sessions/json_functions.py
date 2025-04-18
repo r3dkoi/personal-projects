5c67b3679538dac2156155a1d37bd95c55f93618
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-18 14:46:27 +0800

    fix: escape backslashes in file paths for JSON loading

--- Student Study Tracker/Logging Study Sessions/json_functions.py
+++ Student Study Tracker/Logging Study Sessions/json_functions.py
@@ -4,7 +4,7 @@ import json
 saved_study_sessions = "study_sessions.json"
 
 #Loading subjects from JSON file for tab completion
-with open('Student Study Tracker\Logging Study Sessions\subject_areas.json', 'r') as f:
+with open('Student Study Tracker\\Logging Study Sessions\\subject_areas.json', 'r') as f:
           subject_list = json.load(f)
 
 def subject_completer(text, state):
@@ -15,7 +15,7 @@ def subject_completer(text, state):
     return None
 
 #Loading study_types from JSON file for tab completion
-with open('Student Study Tracker\Logging Study Sessions\study_type.json', 'r') as f:
+with open('Student Study Tracker\\Logging Study Sessions\\study_type.json', 'r') as f:
             study_type_list = json.load(f)
 
 def study_type_completer(text, state):  
