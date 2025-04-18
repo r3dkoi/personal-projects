44ce80e34d0ea3127a783626b87b6a260864526f
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-18 09:42:36 +0800

    fix: Correct test method definitions and improve error handling in test_calculator.py

--- Unit Tests/test_calculator.py
+++ Unit Tests/test_calculator.py
@@ -6,8 +6,15 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5) #This test checks if the output would = to 5
     
-    def test_subtract(sekf):
+    def test_add_negative_numbers_together(self):
+       self.assertEqual(add(-5, -3), -8) #This test checks if output = to -8
+       print("Test pass!")
+    
+    def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2) #Test checks if output = 2
+
+    def test_subtract_answer_equals_negative(self):
+       self.assertEqual(subtract(0, 2), -2) # Test checks if output = -2
     
     def test_multiply(self):
         self.assertEqual(multiply(11,5), 55) #Test checks if output = 55
@@ -16,5 +23,8 @@ class TestCalculator(unittest.TestCase):
      self.assertEqual(divide(9,3), 3) #Test check output = 3
 
     def test_divide_raise_valueError(self):
-       self.assertRaises(ZeroDivisionError):
-       divide(10,0)
+       with self.assertRaises(ZeroDivisionError):
+        divide(10,0)
+
+#Run tests
+unittest.main()
